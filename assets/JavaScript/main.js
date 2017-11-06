37bb16b9720f23e1979eef9ec7480b1b9997bd67
Camila Sáez <cfsaezbravo@gmail.com>
2017-11-06 16:00:58 -0300

    Mapa

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -7,3 +7,45 @@ window.addEventListener('scroll', function (e) {
         	$('.navbar').attr('style', 'background-color: transparent !important');
         }
     });
+
+(function ($) {
+
+			var folsom = new google.maps.LatLng(-33.802073,-70.8534667,17);
+			var markerposition = new google.maps.LatLng(-33.802073,-70.8534667,17);
+
+			var marker;
+			var map;
+
+			function initialize() {
+			  var mapOptions = {
+				zoom: 14,
+				center: folsom,
+                // This is where you would paste any style found on Snazzy Maps.
+				styles: [{featureType:"landscape",stylers:[{saturation:-100},{lightness:65},{visibility:"on"}]},{featureType:"poi",stylers:[{saturation:-100},{lightness:51},{visibility:"simplified"}]},{featureType:"road.highway",stylers:[{saturation:-100},{visibility:"simplified"}]},{featureType:"road.arterial",stylers:[{saturation:-100},{lightness:30},{visibility:"on"}]},{featureType:"road.local",stylers:[{saturation:-100},{lightness:40},{visibility:"on"}]},{featureType:"transit",stylers:[{saturation:-100},{visibility:"simplified"}]},{featureType:"administrative.province",stylers:[{visibility:"off"}]/**/},{featureType:"administrative.locality",stylers:[{visibility:"off"}]},{featureType:"administrative.neighborhood",stylers:[{visibility:"on"}]/**/},{featureType:"water",elementType:"labels",stylers:[{visibility:"on"},{lightness:-25},{saturation:-100}]},{featureType:"water",elementType:"geometry",stylers:[{hue:"#ffff00"},{lightness:-25},{saturation:-97}]}]
+  
+			  };
+
+			  map = new google.maps.Map(document.getElementById('google-map'),
+					  mapOptions);
+
+			  marker = new google.maps.Marker({
+				map:map,
+				draggable:true,
+				animation: google.maps.Animation.DROP,
+				position: markerposition
+			  });
+			  google.maps.event.addListener(marker, 'click', toggleBounce);
+			}
+
+			function toggleBounce() {
+
+			  if (marker.getAnimation() != null) {
+				marker.setAnimation(null);
+			  } else {
+				marker.setAnimation(google.maps.Animation.BOUNCE);
+			  }
+			}
+
+			google.maps.event.addDomListener(window, 'load', initialize);
+	
+})(jQuery);
