bf252d8d7f3df2224de6d8b353d6c67cfe2a6e32
Camila Sáez <cfsaezbravo@gmail.com>
2017-11-06 13:28:56 -0300

    Añadiendo drivers

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function (e) {
 
         if (document.documentElement.scrollTop || document.body.scrollTop > 50) {
-            $('.navbar').attr('style', 'background-color: rgba(175, 60, 229, 1) !important');
+            $('.navbar').attr('style', 'background-color: rgba(175, 60, 229, 0.8) !important');
             }
         else{
         	$('.navbar').attr('style', 'background-color: transparent !important');
